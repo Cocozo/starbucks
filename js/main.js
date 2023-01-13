0ab59255ee98e216a07266f26afe7d0c675f202b
cocozo <kjh062627@gmail.com>
2023-01-13 11:28:49 +0900

    모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener('click', () => {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', () => {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', () => {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl =  document.querySelector("header .badges");
 
 const toTopEl = document.querySelector("#to-top");
@@ -153,5 +136,3 @@ spyEls.forEach((spyEl) => {
 })
 
 
-const thisYear = document.querySelector("footer .this-year");
-thisYear.textContent = new Date().getFullYear();
